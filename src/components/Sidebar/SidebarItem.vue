a462f342583fcf8888bcf844ca20ce16c8dc2bd6
Aituar <aituufs@gmail.com>
2024-03-16 00:46:21 +0100

    Update SidebarItem.vue

--- src/components/Sidebar/SidebarItem.vue
+++ src/components/Sidebar/SidebarItem.vue
@@ -4,9 +4,10 @@ import { useRoute } from 'vue-router'
 import SidebarDropdown from './SidebarDropdown.vue'
 
 const sidebarStore = useSidebarStore()
+const route = useRoute()
 
 const props = defineProps(['item', 'index'])
-const currentPage = useRoute().params.surahId || 1
+const currentPage = +route.params.surahId || 1
 
 interface SidebarItem {
   label: string
@@ -29,7 +30,7 @@ const handleItemClick = () => {
       class="group relative flex items-center gap-2.5 rounded-md py-2 px-4 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4"
       @click.prevent="handleItemClick"
       :class="{
-        'bg-graydark dark:bg-meta-4': sidebarStore.page === item.label
+        'bg-graydark dark:bg-meta-4': currentPage === item.id
       }"
     >
 
