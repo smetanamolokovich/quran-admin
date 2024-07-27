a657f32530299189a3bbd5775c652c007d8308ba
Aituar <aituufs@gmail.com>
2024-07-27 12:22:03 +0200

    ui changes

--- src/views/Dashboard/UpdateVerseView.vue
+++ src/views/Dashboard/UpdateVerseView.vue
@@ -93,21 +93,21 @@ const handleSubmit = () => {
 
 <template>
   <DefaultLayout>
-    <div class="flex flex-col gap-6 mt-4">
-      <div class="w-full h-8 relative">
-        <a v-if="+route.params.ayatId > 1"
-           @click="$router.push(`/surah/${+route.params.id}/ayat/${+route.params.ayatId - 1}`)"
-           class="absolute left-0 cursor-pointer font-bold text-black-600 dark:text-blue-500 hover:underline"
-        >← Аят номер {{ +route.params.ayatId - 1 }}</a>
+    <div class="w-full h-8 relative">
+      <a v-if="+route.params.ayatId > 1"
+         @click="$router.push(`/surah/${+route.params.id}/ayat/${+route.params.ayatId - 1}`)"
+         class="absolute left-0 cursor-pointer font-bold text-black-600 dark:text-blue-500 hover:underline"
+      >← Аят номер {{ +route.params.ayatId - 1 }}</a>
 
-        <a v-if="limit > +route.params.ayatId"
-           @click="$router.push(`/surah/${+route.params.id}/ayat/${+route.params.ayatId + 1}`)"
-           class="absolute right-0 cursor-pointer font-bold text-black-600 dark:text-blue-500 hover:underline"
-        > Аят номер {{ +route.params.ayatId + 1 }} →</a>
-      </div>
-      <div class="flex flex-col gap-5 mb-6">
+      <a v-if="limit > +route.params.ayatId"
+         @click="$router.push(`/surah/${+route.params.id}/ayat/${+route.params.ayatId + 1}`)"
+         class="absolute right-0 cursor-pointer font-bold text-black-600 dark:text-blue-500 hover:underline"
+      > Аят номер {{ +route.params.ayatId + 1 }} →</a>
+    </div>
+    <div class="flex gap-6 mt-4">
+      <div class="flex flex-col gap-5 mb-6 basis-1/4">
         <h1 class="text-2xl font-bold">Перевод:</h1>
-        <Editor v-model="ayat.translation" editorStyle="height: 120px; fontSize: 16px;" @text-change="textChange">
+        <Editor v-model="ayat.translation" editorStyle="height: 60vh; fontSize: 16px; line-height: 1.6;" @text-change="textChange">
           <template v-slot:toolbar>
             <span class="ql-formats">
                 <button v-tooltip.bottom="'Bold'" class="ql-bold"></button>
@@ -117,9 +117,9 @@ const handleSubmit = () => {
           </template>
         </Editor>
       </div>
-      <div class="flex flex-col gap-5">
+      <div class="flex flex-col gap-5 basis-3/4">
         <h1 class="text-2xl font-bold">Тафсир:</h1>
-        <Editor v-model="ayat.tafsir" editorStyle="height: 280px; fontSize: 16px;" @text-change="textTafsirChange" placeholder="Нет тафсира для данного аята...">
+        <Editor v-model="ayat.tafsir" editorStyle="height: 60vh; fontSize: 16px; line-height: 1.6;" @text-change="textTafsirChange" placeholder="Нет тафсира для данного аята...">
           <template v-slot:toolbar>
             <span class="ql-formats">
                 <button v-tooltip.bottom="'Bold'" class="ql-bold"></button>
@@ -129,30 +129,36 @@ const handleSubmit = () => {
           </template>
         </Editor>
       </div>
-      <div class="flex justify-between">
-        <button
-            type="button"
-            class="self-start max-w-[180px] mt-4 focus:outline-none text-white bg-red hover:bg-danger focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
-            @click="$router.push(`/surah/${+route.params.id}`)"
-        >
-          Назад
-        </button>
-        <button
-            v-if="!loading"
-            type="button"
-            class="self-end max-w-[180px] mt-4 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
-            @click="handleSubmit"
-        >
-          Сохранить изменени
-        </button>
-        <button v-else disabled type="button" class="self-end max-w-[180px] mt-4 py-2.5 px-5 me-2 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 focus:z-10 focus:ring-4 focus:outline-none focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 inline-flex items-center">
-          <svg aria-hidden="true" role="status" class="inline w-4 h-4 me-3 text-gray-200 animate-spin dark:text-gray-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
-            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="#1C64F2"/>
-          </svg>
-          Сохранение...
-        </button>
-      </div>
+    </div>
+    <div class="flex justify-between">
+      <button
+          type="button"
+          class="self-start max-w-[180px] mt-4 focus:outline-none text-white bg-red hover:bg-danger focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
+          @click="$router.push(`/surah/${+route.params.id}`)"
+      >
+        Назад
+      </button>
+      <button
+          v-if="!loading"
+          type="button"
+          class="self-end max-w-[200px] mt-4 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+          @click="handleSubmit"
+      >
+        Сохранить изменения
+      </button>
+      <button v-else disabled type="button" class="self-end max-w-[180px] mt-4 py-2.5 px-5 me-2 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 focus:z-10 focus:ring-4 focus:outline-none focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 inline-flex items-center">
+        <svg aria-hidden="true" role="status" class="inline w-4 h-4 me-3 text-gray-200 animate-spin dark:text-gray-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+          <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="#1C64F2"/>
+        </svg>
+        Сохранение...
+      </button>
     </div>
   </DefaultLayout>
 </template>
+<style scoped>
+:deep(.ql-editor p) {
+  font-size: 16px;
+  line-height: 1.6;
+}
+</style>
